fc25475d6fea27e1ec6f52165e49df2dfe979fa1
Wesley Maciel <weslmaciel@gmail.com>
2022-03-21 18:52:53 -0300

    Read data the mysql

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
+// conexão
 $connection = new mysqli('localhost', 'root','','cadastro');
+
+
+// read
+$sql = "select * from peplo";
+
+$results = $connection->query($sql);
+
+if ($results->num_rows){
+    while($peplo = $results->fetch_object()){
+        echo "{$peplo->id} - {$peplo->name} <br>";
+    }
+}else{
+    echo "not content";
+}
