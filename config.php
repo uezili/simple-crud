97ae0c806c445fce5f95e67164c84b5ec57e6d81
Wesley Maciel <weslmaciel@gmail.com>
2022-07-24 13:44:04 -0300

    Restart project

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_NAME', 'hp_crud');
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-
-$dbConnection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';port=' . DB_PORT, DB_USER, DB_PASSWORD);
